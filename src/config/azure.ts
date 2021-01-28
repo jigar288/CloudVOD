8f35e104fb36a6026bb3f3fe6898a1225c1a14cd
Arshad Narmawala <arshadn00@gmail.com>
2021-01-28 13:24:21 -0600

    ✨ feat/Made it into a oop Server (#12)

--- src/config/azure.ts
+++ src/config/azure.ts
@@ -1,33 +1,32 @@
 // Place azure related hard-coded configurations here
 
 interface AzureAccountConfig {
-    AadClientId: string,
-    AadSecret: string,
-    AadTenantDomain: string,
-    AadTenantId: string,
-    AccountName: string,
-    Location: string,
-    ResourceGroup: string,
-    SubscriptionId: string,
-    ArmAadAudience: string,
-    ArmEndpoint: string,
-    activeDirectoryEndpointUrl: string,
+    AadClientId: string
+    AadSecret: string
+    AadTenantDomain: string
+    AadTenantId: string
+    AccountName: string
+    Location: string
+    ResourceGroup: string
+    SubscriptionId: string
+    ArmAadAudience: string
+    ArmEndpoint: string
+    activeDirectoryEndpointUrl: string
 }
 
 // this object is necessary for silencing typescript type restriction
-const azureAccountConfig : AzureAccountConfig = {
-    AadClientId: process.env.AadClientId || 'placeholder',
-    AadSecret: process.env.AadSecret || 'placeholder',
-    AadTenantDomain: process.env.AadTenantDomain || 'placeholder',
-    AadTenantId: process.env.AadTenantId || 'placeholder',
-    AccountName: process.env.AccountName || 'placeholder',
-    Location: process.env.Location || 'placeholder',
-    ResourceGroup: process.env.ResourceGroup || 'placeholder',
-    SubscriptionId: process.env.SubscriptionId || 'placeholder',
+const azureAccountConfig: AzureAccountConfig = {
+    AadClientId: '***REMOVED***',
+    AadSecret: '***REMOVED***',
+    AadTenantDomain: '***REMOVED***',
+    AadTenantId: '***REMOVED***',
+    AccountName: '***REMOVED***',
+    Location: 'North Central US',
+    ResourceGroup: '***REMOVED***',
+    SubscriptionId: '***REMOVED***',
     ArmAadAudience: 'https://management.core.windows.net/',
-    ArmEndpoint: 'https://management.azure.com/',
+    ArmEndpoint: 'https://management.azure.com/%22%7D',
     activeDirectoryEndpointUrl: 'https://login.microsoftonline.com/',
 }
 
-export default azureAccountConfig;
-
+export default azureAccountConfig

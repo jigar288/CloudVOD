e3e30dd10599bda73d180339a889f3861477f5b9
anarmawala <arshadn00@gmail.com>
2021-03-21 16:59:51 -0400

    chore/Move Babel Setup to a seperate file

---
+++ client/babel.config.js
@@ -0,0 +1,14 @@
+require('dotenv-defaults').config({ defaults: true })
+const isDevelopment = process.env.NODE_ENV === 'development'
+
+module.exports = function (api) {
+    api.cache(true)
+
+    const presets = ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript']
+    const plugins = [...(isDevelopment ? ['react-refresh/babel'] : [])]
+
+    return {
+        presets,
+        plugins,
+    }
+}

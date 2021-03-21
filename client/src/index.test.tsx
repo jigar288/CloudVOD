e2098c413abcc6c8e463cd6016f12373407ca01d
anarmawala <arshadn00@gmail.com>
2021-03-21 17:01:59 -0400

    Add jest setup and sanity check

---
+++ client/src/index.test.tsx
@@ -0,0 +1,10 @@
+import {} from 'jest'
+import { cleanup } from '@testing-library/react'
+
+describe('Basic Checks', () => {
+    afterAll(cleanup)
+
+    it('Sanity Check', () => {
+        expect(true).toEqual(true)
+    })
+})

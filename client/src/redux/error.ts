18cea36972d79161c696d0879797e6ad02ebb9e7
Arshad Narmawala <arshadn00@gmail.com>
2021-03-25 14:53:30 -0500

    ✨ feat/Created error reducers

--- client/src/redux/error.ts
+++ client/src/redux/error.ts
@@ -1,8 +1,15 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 import { RootActions } from '.'
 
 export const slice = createSlice({
     name: 'error',
     initialState: {} as { [action in RootActions]: string | undefined },
-    reducers: {},
+    reducers: {
+        remove: (state, action: PayloadAction<RootActions>) => {
+            state[action.payload] = undefined
+        },
+        set: (state, action: PayloadAction<{ err: string; action: RootActions }>) => {
+            state[action.payload.action] = action.payload.err
+        },
+    },
 })

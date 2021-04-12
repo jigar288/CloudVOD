87015e11798f7c6397cd4fcc6c163e4d78b5c462
Jigar Patel <jigar.moyo@gmail.com>
2021-04-12 23:35:39 +0000

    removed thumbnail url from create_initial_video_entry()

--- sql-code/sql-functions-and-procedures/video/create_initial_video_entry.sql
+++ sql-code/sql-functions-and-procedures/video/create_initial_video_entry.sql
@@ -1,6 +1,6 @@
 DROP PROCEDURE IF EXISTS create_initial_video_entry;
 
-CREATE PROCEDURE create_initial_video_entry(title VARCHAR, description VARCHAR, given_output_asset_name VARCHAR, upload_date DATE, category_IDs int[], user_email VARCHAR, user_profile_url VARCHAR, name VARCHAR, is_public BOOLEAN, thumbnail_url VARCHAR)
+CREATE PROCEDURE create_initial_video_entry(title VARCHAR, description VARCHAR, given_output_asset_name VARCHAR, upload_date DATE, category_IDs int[], user_email VARCHAR, user_profile_url VARCHAR, name VARCHAR, is_public BOOLEAN)
 LANGUAGE plpgsql
 AS $$
 DECLARE userID INTEGER := get_user_id(user_email);
@@ -14,7 +14,7 @@ BEGIN
     END IF;     
 
     -- create db record for all metadata
-    INSERT INTO public.video (title, description, output_asset_name, upload_date, is_public, thumbnail_url, user_id) VALUES (title, description, given_output_asset_name::uuid, upload_date, is_public, thumbnail_url, userID);    
+    INSERT INTO public.video (title, description, output_asset_name, upload_date, is_public, user_id) VALUES (title, description, given_output_asset_name::uuid, upload_date, is_public, userID);    
     
     -- get video_id
     SELECT id INTO video_id FROM public.video WHERE output_asset_name=given_output_asset_name::uuid;
@@ -22,5 +22,5 @@ BEGIN
     CALL enter_video_categories(video_id, category_IDs);
 END; $$;
 
--- CALL create_initial_video_entry('Demon Slayer Trailer', 'Join Tanjoro on his journey to slay demons', 'bf2f390e-9a56-11eb-a8b3-0242ac130003', '2019-02-02', ARRAY [ 2,1 ], 'jpate218@uic.edu', 'photos.google.com/jigar-profile', 'JP', TRUE, 'www.google.com/thumbnail-url')
+-- CALL create_initial_video_entry('Demon Slayer Trailer', 'Join Tanjoro on his journey to slay demons', 'bf2f390e-9a56-11eb-a8b3-0242ac130003', '2019-02-02', ARRAY [ 2,1 ], 'jpate218@uic.edu', 'photos.google.com/jigar-profile', 'JP', TRUE)
 

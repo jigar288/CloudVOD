8581bb825360b7501e51a5011e316575b0805188
Arshad Narmawala <arshadn00@gmail.com>
2021-04-11 23:47:33 -0500

    ✨ feat/Created base componnents

---
+++ client/src/pages/VideosPage.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react'
+
+export const VideosPage = () => {
+    return <>VideosPage</>
+}
--- client/src/utilities/paths.tsx
+++ client/src/utilities/paths.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import { AiOutlineLogin, AiOutlineLogout, AiOutlineUpload, AiOutlineVideoCamera } from 'react-icons/ai'
+import { AiOutlineLogin, AiOutlineLogout, AiOutlineUpload, AiOutlineVideoCamera, AiOutlineInfoCircle } from 'react-icons/ai'
 import { NavbarPath } from '../types'
+import { APP_ROUTES } from './routes'
 
 export const paths: NavbarPath[] = [
-    { name: 'Videos', icon: <AiOutlineVideoCamera />, href: '/' },
-    { name: 'Upload', icon: <AiOutlineUpload />, authenticated: true, href: '/upload' },
+    { name: 'Videos', icon: <AiOutlineVideoCamera />, href: APP_ROUTES.VIDEOS_PAGE },
+    { name: 'Upload', icon: <AiOutlineUpload />, authenticated: true, href: APP_ROUTES.UPLOAD_PAGE },
     { name: 'Login', icon: <AiOutlineLogin />, authenticated: false, href: '/', external: true },
     { name: 'Sign Off', icon: <AiOutlineLogout />, authenticated: true, href: '/', external: true },
+    { name: 'About Us', icon: <AiOutlineInfoCircle />, href: APP_ROUTES.ABOUT_PAGE },
 ]

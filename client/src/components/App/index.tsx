d04b555639117e667bd1707b023a45822dad384f
Arshad Narmawala <arshadn00@gmail.com>
2021-04-11 22:28:49 -0500

    ✨ Added routes rendering

--- client/src/components/App/index.tsx
+++ client/src/components/App/index.tsx
@@ -1,13 +1,35 @@
 import * as React from 'react'
-import Cards from '../Cards'
 import Navbar from '../Navbar'
-import VideoPlayer from '../Video'
-import { paths } from '../../utilities'
+import { HashRouter, Route } from 'react-router-dom'
+import { paths, routes } from '../../utilities'
+import { useAppDispatch, useAppSelector } from '../../redux/store'
+import { check_service } from '../../redux/sanity'
+import { authorize } from '../../redux/user'
 
 export const App = () => {
+    const dispatch = useAppDispatch()
+    const { user, sanity } = useAppSelector((state) => state)
+
+    React.useEffect(() => {
+        dispatch(check_service())
+        dispatch(authorize())
+    }, [])
+
     return (
-        <>
+        <HashRouter>
             <Navbar paths={paths} />
-        </>
+
+            {JSON.stringify(user)}
+            {JSON.stringify(sanity)}
+            {routes.map(({ path, exact, component, authenticated, dependent }, idx) => {
+                const displaySanityError = dependent && !sanity
+                const displayAuthError = !displaySanityError && authenticated && user === null
+
+                // TODO: Fix which component to display
+                const displayComponent = displaySanityError ? component : displayAuthError ? component : component
+
+                return <Route path={path} exact={exact} component={displayComponent} key={idx} />
+            })}
+        </HashRouter>
     )
 }

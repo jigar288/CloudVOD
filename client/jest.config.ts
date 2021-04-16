f658d888ac224f22b05adb6d639a3173c51c9ec4
Clark Chen <9372896+clarkchentw@users.noreply.github.com>
2021-04-16 12:01:40 -0500

    Fix jest compile issue

--- client/jest.config.ts
+++ client/jest.config.ts
@@ -1,9 +1,14 @@
-import path from 'path'
+import * as path from 'path'
 import type { Config } from '@jest/types'
 
 // Sync object
 const config: Config.InitialOptions = {
+    preset: 'ts-jest',
+    transform: {
+        '^.+\\.(ts|tsx)?$': 'ts-jest',
+        "^.+\\.(js|jsx)$": "babel-jest",
+    },
     setupFilesAfterEnv: [path.resolve(__dirname, 'src/tests/test.setup.ts')],
-    verbose: true,
+    verbose: true
 }
 export default config

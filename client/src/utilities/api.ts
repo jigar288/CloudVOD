885085b6fe177650601f8d257ea70a42a21c0daf
Arshad Narmawala <arshadn00@gmail.com>
2021-04-21 17:16:29 -0500

    Fix

--- client/src/utilities/api.ts
+++ client/src/utilities/api.ts
@@ -1,7 +1,7 @@
 import Axios from 'axios'
 
 export const api = Axios.create({
-    baseURL: '/api',
+    baseURL: process.env.NODE_ENV === 'development' || !process.env.API_URL || !process.env.API_BASE_PATH ? '/api' : process.env.API_URL + process.env.API_BASE_PATH,
     withCredentials: true,
     validateStatus: () => true,
 })

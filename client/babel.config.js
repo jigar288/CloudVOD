903bcf2fc9212aab391d547a8750738c90e45a99
Arshad Narmawala <arshadn00@gmail.com>
2021-04-25 12:16:07 -0500

    🐛 chore/Remove fsevents + ts-jest and use babel

--- client/babel.config.js
+++ client/babel.config.js
@@ -1,11 +1,12 @@
 require('dotenv-defaults').config({ defaults: true })
 const isDevelopment = process.env.NODE_ENV !== 'production'
+const isTest = process.env.NODE_ENV === 'test'
 
 module.exports = function (api) {
     api.cache(true)
 
-    const presets = [['@babel/preset-env', { modules: false, targets: { esmodules: true } }], '@babel/preset-typescript', '@babel/preset-react']
-    const plugins = [...(isDevelopment ? ['react-refresh/babel'] : [])]
+    const presets = [['@babel/preset-env', { modules: !isTest }], '@babel/preset-typescript', '@babel/preset-react']
+    const plugins = [isDevelopment && !isTest && 'react-refresh/babel'].filter(Boolean)
 
     return {
         presets,
--- client/jest.config.ts
+++ client/jest.config.ts
@@ -3,12 +3,7 @@ import type { Config } from '@jest/types'
 
 // Sync object
 const config: Config.InitialOptions = {
-    preset: 'ts-jest',
-    transform: {
-        '^.+\\.(ts|tsx)?$': 'ts-jest',
-        "^.+\\.(js|jsx)$": "babel-jest",
-    },
     setupFilesAfterEnv: [path.resolve(__dirname, 'src/tests/test.setup.ts')],
-    verbose: true
+    verbose: true,
 }
 export default config
